124b22347ec4d358ab9ec729d4c1e2626ac6d76d
anonymze <anodevfr@gmail.com>
2025-08-28 22:44:39 +0200

    base PAYLOAD CMS

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,6 @@
 // storage-adapter-import-placeholder
 import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
+// import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
