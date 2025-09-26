1a01e5d13073a11f86f2e596b80c5b9fdafa599b
anonymze <anodevfr@gmail.com>
2025-09-27 01:49:40 +0200

    add: config

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,15 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+import { withPayload } from '@payloadcms/next/withPayload';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
-  webpack: (webpackConfig) => {
-    webpackConfig.resolve.extensionAlias = {
-      '.cjs': ['.cts', '.cjs'],
-      '.js': ['.ts', '.tsx', '.js', '.jsx'],
-      '.mjs': ['.mts', '.mjs'],
-    }
-
-    return webpackConfig
-  },
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/admin",
+      permanent: true,
+    },
+  ],
 }
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default withPayload(nextConfig)
